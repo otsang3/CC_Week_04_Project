55339d006880a27e5e6610340b44c3c4888e3331
Oscar Tsang <ohhtsang@gmail.com>
2020-04-03 21:14:54 +0100

    amended edit to team in appropriate methods

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('./controllers/edit_controller')
+require_relative('./controllers/team_controller')
 require_relative('./controllers/fixtures_controller')
 require_relative('./controllers/stats_controller')
 require_relative('./controllers/table_controller')
