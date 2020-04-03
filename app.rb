26ef7e2790cd12634f9e80b903993d41967a9e8e
Oscar Tsang <ohhtsang@gmail.com>
2020-04-03 18:41:13 +0100

    removed background image

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require_relative('./controllers/edit_controller')
 require_relative('./controllers/fixtures_controller')
 require_relative('./controllers/stats_controller')
 require_relative('./controllers/table_controller')
+also_reload('./models/*')
 
 get '/' do
   erb(:index)
