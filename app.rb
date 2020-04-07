3180eca60202cbedaa21c1231bf50f5bbb0d82cd
Oscar Tsang <ohhtsang@gmail.com>
2020-04-07 12:24:45 +0100

    amended some file names as per naming convention

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('./controllers/results_controller')
-require_relative('./controllers/team_controller')
+require_relative('./controllers/teams_controller')
 require_relative('./controllers/fixtures_controller')
 require_relative('./controllers/stats_controller')
-require_relative('./controllers/table_controller')
+require_relative('./controllers/tables_controller')
 also_reload('./models/*')
 
 get '/' do
